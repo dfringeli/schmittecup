4ebde36ede59bd33a7f035e84fba7e63cc9f467a
Dominik Fringeli <dfringeli@outlook.com>
2020-05-05 17:57:17 +0200

    adjust footer

--- schmittecup/footer.php
+++ schmittecup/footer.php
@@ -52,43 +52,13 @@
 
 							?>
 
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>">TV Zwingen</a>
+							<a href="https://tvzwingen.ch">TV Zwingen</a>
 
 						</p><!-- .footer-copyright -->
 
 
 
 					</div><!-- .footer-credits -->
-
-
-
-					<a class="to-the-top" href="#site-header">
-
-						<span class="to-the-top-long">
-
-							<?php
-
-							/* translators: %s: HTML character for up arrow. */
-
-							printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-
-							?>
-
-						</span><!-- .to-the-top-long -->
-
-						<span class="to-the-top-short">
-
-							<?php
-
-							/* translators: %s: HTML character for up arrow. */
-
-							printf( __( 'Up %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-
-							?>
-
-						</span><!-- .to-the-top-short -->
-
-					</a><!-- .to-the-top -->
 
 
 
